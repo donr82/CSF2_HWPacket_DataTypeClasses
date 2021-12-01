91f69a33b73fa24c8e69b71d95ff3e7781bc66db
Don Richardson <donrichardson092388@outlook.com>
2021-11-30 22:07:37 -0600

    completed student lab set

---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+        /*
+         * 1. Student
+         *  a. firstName - string
+         *  b. lastName - string
+         *  c. id - string
+         *  d. gpa - float
+         *  
+         *  Include all fields, properties, two constructors (default and fully  qualified),
+         *  and a ToString() for each class. Then test the class you created in the TesterProgram
+         *  project by creating objects using both constructors and writing them on the screen.
+         *  
+         *  Merge each completed branch back to the master branch and push master to the remote repo
+         */
+
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }//end FirstName
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }//end LastName
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }//end id
+
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }//end Gpa
+
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }//end fq ctor
+
+        public Student() { }
+
+        public override string ToString()
+        {
+            return string.Format($"FirstName: {FirstName}\n" +
+                                 $"LastName: {LastName}\n" +
+                                 $"Id: {Id}\n" +
+                                 $"Gpa: {Gpa:n0}\n");
+        }//end ToString
+
+    }//end class
+}//end namespace
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
 
 namespace TesterProgram
 {
@@ -10,6 +11,18 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            Console.Title = "CSF2 Homework Packet";
+            Console.WriteLine("CSF2 Homework Packet for Data Type Classes");
+
+            Console.WriteLine("\n\nStudent Data Type Lab Set");
+
+            Student s1 = new Student();
+            s1.FirstName = "Han";
+            s1.LastName = "Solo";
+            s1.Id = "Millennium Falcon";
+            s1.Gpa = 3.89f;
+
+            Console.WriteLine($"{s1.FirstName} {s1.LastName} is our new student. His id is {s1.Id} and his GPA is {s1.Gpa}.");
         }
     }
 }
