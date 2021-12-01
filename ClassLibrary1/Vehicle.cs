80f77e449a4062ba7478fc8ff8e9913c907856f9
Don Richardson <donrichardson092388@outlook.com>
2021-11-30 22:49:22 -0600

    completed Vehicle Lab Set

---
+++ ClassLibrary1/Vehicle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Vehicle
+    {
+        /*
+         * 2. Vehicle
+         *  a. make - string
+         *  b. model - string
+         *  c. year - int
+         *  d. weight - float
+         *  
+         *  Include all fields, properties, two constructors (default and fully  qualified),
+         *  and a ToString() for each class. Then test the class you created in the TesterProgram
+         *  project by creating objects using both constructors and writing them on the screen.
+         *  
+         *  Merge each completed branch back to the master branch and push master to the remote repo
+         */
+
+        private string _make;
+        private string _model;
+        private int _year;
+        private float _weight;
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public float Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public Vehicle(string make, string model, int year, float weight)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Weight = weight;
+        }
+
+        public Vehicle() { }
+
+        public override string ToString()
+        {
+            return string.Format($"Make: {Make}/n" +
+                                 $"Model: {Model}\n" +
+                                 $"Year: {Year}\n" +
+                                 $"Weight: {Weight:n0}\n");
+        }
+
+    }
+}
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -12,7 +12,8 @@ namespace TesterProgram
         static void Main(string[] args)
         {
             Console.Title = "CSF2 Homework Packet";
-            Console.WriteLine("CSF2 Homework Packet for Data Type Classes");
+
+            Console.WriteLine("CSF2 Homework Packet for Data Type Classes");            
 
             Console.WriteLine("\n\nStudent Data Type Lab Set");
 
@@ -23,6 +24,17 @@ namespace TesterProgram
             s1.Gpa = 3.89f;
 
             Console.WriteLine($"{s1.FirstName} {s1.LastName} is our new student. His id is {s1.Id} and his GPA is {s1.Gpa}.");
+
+            Console.WriteLine("\n\nVehicle Lab Set\n\n");
+
+            Vehicle v1 = new Vehicle();
+            v1.Make = "DeLorean";
+            v1.Model = "DMC - 12";
+            v1.Year = 1976;
+            v1.Weight = 2743;
+
+            Console.WriteLine($"Doc Brown made a time machine out of a { v1.Year}  { v1.Make} { v1.Model}.  " +
+                $"He somehow got it to its speed of 88mph eventhough it weighed {v1.Weight:n0}lbs.");
         }
     }
 }
